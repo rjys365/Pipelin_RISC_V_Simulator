323067daac0b48821d404a68aad71edf4f145257
rjys365 <rjys365@hotmail.com>
2022-07-04 02:17:41 +0800

    Revert "修正返回栈"
    This reverts commit ed831878f830a228bedb1f22e909ca42035a80df.

--- predictor.hpp
+++ predictor.hpp
@@ -2,106 +2,10 @@
 #define PREDICTOR_HPP
 #include<cstdint>
 #include<utility>
-#include<stack>
-#include<queue>
-#include<iostream>
-#define ENABLE_PREDICTION
-#define ENABLE_RAS
 class Predictor{
-private:
-    enum class TwoBitState{N=0,NN=1,T=2,TT=3};
-    static const int PRED_SIZE=65536*8/4;
-    TwoBitState cmdStates[PRED_SIZE][16]={(TwoBitState)0};
-    uint8_t cmdPt[PRED_SIZE]={0};
-    uint8_t jaljalr[PRED_SIZE]={0};//1:jal 2:jalr
-    uint32_t topc[PRED_SIZE]={0};
-    std::stack<uint32_t>retstk;
-    uint32_t retstk_popval=(uint32_t)(-1);
-    uint32_t correctCnt=0,totalCnt=0;
-    //uint32_t hispc[2]={0};
 public:
-    std::pair<bool,uint32_t> get(int lastpc){
-#ifndef ENABLE_PREDICTION
-        return std::make_pair(false,0);
-#endif
-        lastpc>>=2;
-        
-        if(jaljalr[lastpc]==2){//jalr
-#ifndef ENABLE_RAS
-            return std::make_pair(false,0);
-#endif
-            if(retstk.empty())return std::make_pair(false,0);//unable to predict
-            uint32_t ret=retstk.top();
-            retstk_popval=ret;
-            retstk.pop();
-            return std::make_pair(true,ret);
-        }
-        retstk_popval=(uint32_t)(-1);
-        if(jaljalr[lastpc]==1){//jal
-            //retstk[]
-            return std::make_pair(true,topc[lastpc]);//TODO
-        }
-        if(cmdStates[lastpc][cmdPt[lastpc]]==TwoBitState::T||cmdStates[lastpc][cmdPt[lastpc]]==TwoBitState::TT){
-            return std::make_pair(true,topc[lastpc]);
-        }
-        return std::make_pair(false,0);
-
-    }
-    void feed(uint32_t frompc,uint32_t topc,bool isbn,bool isjal,bool isjalr){//true: jumped
-#ifndef ENABLE_PREDICTION
-        if(isbn){
-            totalCnt++;
-            if(topc==frompc+4)correctCnt++;
-        }
-        return;
-#endif
-        //if(frompc==0x10a0)
-        //    std::cout<<"AAAAAAAAAA!!!!!!!!"<<std::endl;
-        if(!(isbn||isjal||isjalr))return;//not a branch, don't update
-        uint32_t kfrompc=(frompc>>2);
-        //frompc>>=2;
-        if(isjal){
-            //if(topc==this->topc[frompc])correctCnt++;
-            retstk.push(frompc+4);
-            this->topc[kfrompc]=topc;
-            jaljalr[kfrompc]=1;
-            return;
-        }
-        if(isjalr){
-#ifndef ENABLE_RAS
-            return;
-#endif
-            if(jaljalr[kfrompc]!=2&&!retstk.empty())retstk.pop();
-            jaljalr[kfrompc]=2;
-            return;
-        }
-        totalCnt++;
-        uint32_t ptopc=((int)cmdStates[kfrompc][cmdPt[kfrompc]]>1)?this->topc[kfrompc]:(frompc+4);
-        if(topc==ptopc)correctCnt++;
-        else{
-            if(retstk_popval!=(uint32_t)-1)retstk.push(retstk_popval);
-        }
-        bool taken=(topc!=frompc+4);
-        if(taken)this->topc[kfrompc]=topc;
-        auto &st=cmdStates[kfrompc][cmdPt[kfrompc]];
-        switch(st){
-            case TwoBitState::N:
-                st=taken?TwoBitState::T:TwoBitState::NN;
-                break;
-            case TwoBitState::NN:
-                st=taken?TwoBitState::N:TwoBitState::NN;
-                break;
-            case TwoBitState::T:
-                st=taken?TwoBitState::TT:TwoBitState::N;
-                break;
-            case TwoBitState::TT:
-                st=taken?TwoBitState::TT:TwoBitState::T;
-                break;
-        }
-        cmdPt[kfrompc]=(((cmdPt[kfrompc]<<1)|taken)&15);
-    }
-    void printCorrectness(){
-        std::cout<<""<<(double)correctCnt/(double)totalCnt<<"\t"<<correctCnt<<"\t"<<totalCnt<<std::endl;
+    std::pair<bool,uint32_t> get(int expc){return std::make_pair(false,0);}
+    void feed(uint32_t frompc,uint32_t topc){//true: jumped
     }
 };
 
