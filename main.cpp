1a810ea523f6d2805b80339ed2b37dad27b7f241
rjys365 <rjys365@hotmail.com>
2022-07-03 13:09:46 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include"PipelineComputer.hpp"
-ppca::PipeLineComputer computer();
+ppca::PipeLineComputer computer("");
 //ppca::PipeLineComputer computer("array_test1.data");
 //ppca::SequentialComputer computer("array_test1.data");
 //ppca::SequentialComputer computer("pi.data");
